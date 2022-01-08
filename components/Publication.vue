2a19c641631a5ca2e974045d0d736e501502a89e
Félix <hucoxileflagon@gmail.com>
2022-01-08 18:27:12 +0100

    Initial commit

---
+++ components/Publication.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <div class="px-4">
+          <a :href="publication.link" target="_blank">
+              <div class="flex flex-wrap items-center p-2 my-8 -mx-4 rounded-md shadow-md bg-zinc-200 hover:shadow-card">
+                  <div class="w-full px-4 my-2 text-2xl font-bold text-center md:w-2/6">
+                      {{ publication.title }}
+                  </div>
+                  <div class="w-full px-4 md:w-4/6">
+                      <div class="mb-1">
+                          <span class="text-lg font-bold">{{ publication.publisher }}</span> || {{ publication.date }}
+                      </div>
+                      <p class="leading-relaxed text-justify">
+                        {{ publication.description }}
+                      </p>
+                  </div>
+              </div>
+          </a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PublicationCard',
+  props: {
+    publication: {
+      type: Object,
+      required: true
+    }
+  },
+};
+</script>
