04cdee4dff9a0463fc28311d2e9ad84c6d0311cf
Félix <hucoxileflagon@gmail.com>
2022-01-09 21:30:17 +0100

    Add netlify identity widget

--- pages/index.vue
+++ pages/index.vue
@@ -17,5 +17,12 @@
 <script>
 export default {
   name: 'IndexPage',
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    };
+  },
 };
 </script>
